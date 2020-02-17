5fce619c652d61f606bb60e1813c2b974dc19f95
adamrysyk <rysyk.adam@gmail.com>
2020-02-17 14:47:14 -0500

    add headline class component with tests

---
+++ src/components/headline/headline.spec.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Headline from './index';
+import { findByTestAttr } from '../../../utils';
+
+const setup = (props={}) => {
+    const component = shallow(<Headline {...props} />);
+    return component;
+}
+
+describe('Headline Component', () => {
+
+    describe('Have props', () => {
+        let wrapper;
+        beforeEach(() => {
+            const props = {
+                header: 'test header',
+                desc: 'test desc',
+            };
+            wrapper = setup(props);
+        })
+
+        it('Should render without errors', () => {
+            const component = findByTestAttr(wrapper, 'headline');
+            expect(component.length).toBe(1);
+        });
+
+        it('Should render a header', () => {
+            const component = findByTestAttr(wrapper, 'header');
+            expect(component.length).toBe(1);
+        });
+
+
+        it('Should render a description', () => {
+            const component = findByTestAttr(wrapper, 'desc');
+            expect(component.length).toBe(1);
+        });
+
+    });
+
+    describe('Have no props', () => {
+        let wrapper;
+        beforeEach(() => {
+            wrapper = setup();
+        })
+
+        it('Should not render', () => {
+            const component = findByTestAttr(wrapper, 'headline');
+            expect(component.length).toBe(0);
+        });
+    });
+
+});
